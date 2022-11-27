3f44659f0e8d1c73c767266f6db782d30a041740
michal-miziolek-fiedorczuk <9918405+michal-miziolek-fiedorczuk@users.noreply.github.com>
2022-11-27 16:55:57 +0100

    added hello.py

---
+++ hello.py
@@ -0,0 +1,15 @@
+# Function to print the message
+def print_message(message):
+    print(message)
+    return message
+
+
+def subtract(num_a, num_b):
+    result = num_a - num_b
+    print(result)
+    return result
+
+
+print_message("Hello world")
+
+subtract(2, 4)
